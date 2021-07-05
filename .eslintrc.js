da6236d98909864b4ef2e1ab8b33a791a2381053
YangYang <ss880309@gmail.com>
2021-07-05 16:45:34 +0800

    initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/prop-types': [0],
+    'react/jsx-props-no-spreading': [0],
+    'arrow-body-style': 0,
+    'no-console': 0,
+    'max-len': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ shared/components/navigation/index.jsx
@@ -0,0 +1,19 @@
+import { css } from '@emotion/react';
+
+const style = css`
+  color: hotpink;
+`;
+
+// const anotherStyle = css({
+//     textDecoration: 'underline'
+//   })
+
+const Navigation = () => {
+  return (
+    <div css={style}>
+      test
+    </div>
+  );
+};
+
+export default Navigation;
