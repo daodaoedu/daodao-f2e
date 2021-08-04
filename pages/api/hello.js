79af95b98e18a36176e547388af6114a985cb929
YangYang <ss880309@gmail.com>
2021-08-04 19:39:51 +0800

    加備註

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// 目前使用Next產生靜態網頁，原則上這裡不會需要用到
+// 以後架後端的時候再考慮使用
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' });
 }
