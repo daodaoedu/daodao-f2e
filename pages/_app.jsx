42961b1c36dffb05810c8be10cdf552769f817f6
yanglin <ss880309@gmail.com>
2022-02-28 00:52:38 +0800

    新增GA

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Toaster } from "react-hot-toast";
@@ -6,12 +6,34 @@ import { Provider } from "react-redux";
 import GlobalStyle from "../shared/styles/Global";
 import theme from "../shared/styles/theme";
 import storeFactory from "../utils/configureStore";
+import { initGA, logPageView } from "../utils/analytics";
+import { useRouter } from "next/router";
+
 // import Navigation from "../shared/components/Navigation";
 // import Footer from "../shared/components/Footer";
 
 const store = storeFactory();
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter();
+  useEffect(() => {
+    initGA();
+    // `routeChangeComplete` won't run for the first page load unless the query string is
+    // hydrated later on, so here we log a page view if this is the first render and
+    // there's no query string
+    if (!router.asPath.includes("?")) {
+      logPageView();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    // Listen for page changes after a navigation or when the query changes
+    router.events.on("routeChangeComplete", logPageView);
+    return () => {
+      router.events.off("routeChangeComplete", logPageView);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider theme={theme}>
       {/* mui normalize css */}
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import Document, {
-  Html, Head, Main, NextScript,
-} from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -12,6 +11,21 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="zh-Hant">
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-9Z1P1RKY69"
+        />
+        <Script>
+          {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=UA-181407006-3"
+          ></script>
+          <script>
+            window.dataLayer = window.dataLayer || []; dataLayer.push('js', new
+            Date()); dataLayer.push('config', 'UA-181407006-3');
+          </script>
+        </Script>
         <Head />
         <body>
           <Main />
