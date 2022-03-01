dd1d45e778439dbc1b9d9796ff2d48ab120eea3e
yanglin <ss880309@gmail.com>
2022-03-01 21:22:05 +0800

    拿掉線上環境的log

--- utils/configureStore.js
+++ utils/configureStore.js
@@ -1,16 +1,23 @@
-import { createStore, applyMiddleware, compose } from 'redux';
-import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger';
-import reducers from '../redux/reducers';
-import rootSaga from '../redux/sagas';
-
-const sagaMiddleware = createSagaMiddleware();
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import logger from "redux-logger";
+import reducers from "../redux/reducers";
+import rootSaga from "../redux/sagas";
 
 const configureStore = () => {
-  const store = compose(
-    applyMiddleware(sagaMiddleware, logger),
-    // window && window.devToolsExtension && window.devToolsExtension(),
-  )(createStore)(reducers);
+  const isDev = process.env.NODE_ENV === "development";
+  // 1. 產生 redux-saga middleware
+  const sagaMiddleware = createSagaMiddleware();
+
+  // 2. 產生 store並且串接 redux middleware
+  const store = isDev
+    ? compose(
+        applyMiddleware(sagaMiddleware, logger)
+        // window && window.devToolsExtension && window.devToolsExtension(),
+      )(createStore)(reducers)
+    : applyMiddleware(sagaMiddleware)(createStore)(reducers);
+
+  // 3. 執行middleware
   sagaMiddleware.run(rootSaga);
   return store;
 };
