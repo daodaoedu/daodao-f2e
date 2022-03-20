788005993d29b348bda532727f2fb6d76c185d7f
yanglin <ss880309@gmail.com>
2022-03-20 15:26:23 +0800

    更新文案

--- pages/search/index.jsx
+++ pages/search/index.jsx
@@ -50,15 +50,14 @@ const SearchPage = () => {
   }, [router?.query?.cats, router?.query?.q, router?.query?.tags]);
   const SEOData = useMemo(
     () => ({
-      title: `${title}學習資源列表｜島島阿學`,
+      title: `${title}多元學習資源列表｜島島阿學`,
       description:
-        "「島島阿學」盼能透過建立學習資源網絡，讓自主學習者能找到合適的成長方法，進而成為自己想成為的人，並從中培養共好精神。目前正積極打造「可共編的學習資源平台」。",
+        "「島島阿學」盼能透過建立多元的學習資源網絡，讓自主學習者能找到合適的成長方法，進一步成為自己想成為的人，從中培養共好精神。目前正積極打造「可共編的學習資源平台」。",
       keywords: "島島阿學",
       author: "島島阿學",
       copyright: "島島阿學",
       imgLink: "https://www.daoedu.tw/preview.webp",
       link: `${process.env.HOSTNAME}${router?.asPath}`,
-      // root才需要放這個，避免影響到其他頁面
       structuredData: {
         "@context": "https://schema.org",
         "@type": "Course",
