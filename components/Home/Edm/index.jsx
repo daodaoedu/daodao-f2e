2d167f6bb1f75058f112f445484b71e1e13d12ac
vincent <innocent0705@gmail.com>
2023-02-25 15:49:44 +0800

    add Edm section on Home

---
+++ components/Home/Edm/index.jsx
@@ -0,0 +1,108 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Button, Box, Typography,Link } from '@mui/material';
+import { bgcolor } from '@mui/system';
+
+const EdmWrapper = styled.div`
+  width: 90%;
+  /* height: calc(var(--section-height) + var(--section-height-offset)); */
+  margin: 0 auto;
+  padding-top: 40px;
+  padding-bottom: 120px;
+  @media (max-width: 767px) {
+    padding-top: 40px;
+    padding-bottom: 20px;
+  }
+`;
+
+function Edm() {
+  return (
+    <EdmWrapper>
+      <Box
+        sx={{
+          paddingTop: '60px',
+          paddingBottom: '8px',
+          bgcolor: '#DEF5F5',
+          borderRadius: '24px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          '@media(max-width: 767px)': {
+            paddingLeft:'5%',
+            paddingRight:'5%',
+          },
+        }}
+      >
+        <Typography
+          variant="h2"
+          sx={{
+            color: '#293A3D',
+            fontWeight: 'bold',
+            fontSize: '36px',
+            lineHeight: '140%',
+            letterSpacing: '0.08em',
+            '@media(max-width: 767px)': {
+              fontSize: '26px',
+            },
+          }}
+        >
+          想收到最新資訊嗎？
+        </Typography>
+        <Typography
+          variant="h3"
+          sx={{
+            paddingTop: '8px',
+            color: '#293A3D',
+            fontWeight: 'bold',
+            fontSize: '18px',
+            lineHeight: '140%',
+            letterSpacing: '0.08em',
+            '@media(max-width: 767px)': {
+              fontSize: '14px',
+            },
+          }}
+        >
+          歡迎訂閱島島電子報
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            maxWidth:'370px',
+            paddingTop: '24px',
+            paddingBottom: '24px',
+            color: '#536166',
+            fontWeight: 'bold',
+            fontSize: '14px',
+            lineHeight: '140%',
+            letterSpacing: '0.08em',
+            textAlign: 'center',
+            '@media(max-width: 767px)': {
+              fontSize: '8px',
+            },
+          }}
+        >
+          每月與您分享最新資訊，內容包含：國內外教育新聞、自學經驗分享、實驗教育職缺、每月最新自學資源
+        </Typography>
+        <Link href="https://forms.gle/wXuKYmQjzRKRxe387">
+        <Button
+          variant="contained"
+          sx={{
+            width: '200px',
+            padding: '9px 60px 9px 60px',
+            marginBottom: '60px',
+            bgcolor: '#16B9B3',
+            color: '#FFFFFF',
+            borderRadius: '20px',
+            fontSize: '16px',
+          }}
+        >
+          訂閱電子報
+        </Button>
+        </Link>
+      </Box>
+    </EdmWrapper>
+  );
+}
+
+export default Edm;
