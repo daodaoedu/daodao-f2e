6759519c1b089584bc8252e03c4d20a7d1a1b3ab
yanglin <yanglin@fa>
2023-03-03 00:17:26 +0800

    update banner

--- components/Home/index.jsx
+++ components/Home/index.jsx
@@ -8,10 +8,11 @@ import Group from './Group';
 import Edm from './Edm';
 import FacebookPosts from './FacebookPosts';
 import WishResource from './WishResource';
+import ComingSoon from './ComingSoon';
 
 const HomeWrapper = styled.div``;
 
-const Home = () => {
+function Home() {
   const guideRef = useRef(null);
   return (
     <HomeWrapper>
@@ -24,11 +25,13 @@ const Home = () => {
       <Divider sx={{ margin: '10px 0' }} />
       <WishResource />
       <Divider sx={{ margin: '10px 0' }} />
+      <ComingSoon />
+      <Divider sx={{ margin: '10px 0' }} />
       <div ref={guideRef} />
       <Guide />
       <Edm />
     </HomeWrapper>
   );
-};
+}
 
 export default Home;
