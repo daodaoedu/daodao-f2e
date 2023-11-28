a538925f8f4e79935f26e5aa7330c818d9b2aea7
hsuifang <whalekiller03@gmail.com>
2023-11-28 22:35:13 +0800

    feat(Partner/Banner): Modify Banner and Include shared image

--- components/Partner/Banner/index.jsx
+++ components/Partner/Banner/index.jsx
@@ -1,71 +1,82 @@
-import React from 'react';
-import { Box, Button, Typography } from '@mui/material';
+import styled from '@emotion/styled';
 import { useRouter } from 'next/router';
-import BannerImage from './BannerImage';
+import { Box } from '@mui/material';
+import Button from '@/shared/components/Button';
+import Image from '@/shared/components/Image';
+import partnerImg from '@/public/assets/partner-banner.png';
+
+const StyledBanner = styled(Box)(({ theme }) => ({
+  height: '60vh',
+  zIndex: 0,
+  position: 'relative',
+  [theme.breakpoints.down('md')]: {
+    height: '40vh',
+  },
+  '> picture': {
+    position: 'absolute',
+    width: '100%',
+    top: 0,
+    height: '398px',
+    zIndex: -1,
+    [theme.breakpoints.down('md')]: {
+      height: '256px',
+    },
+    img: {
+      height: 'inherit',
+    },
+  },
+}));
+
+const StyledContent = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  [theme.breakpoints.down('md')]: {
+    padding: '40px 20px 0',
+  },
+  '> h1': {
+    fontWeight: 700,
+    fontSize: '36px',
+    lineHeight: '140%',
+    color: '#536166',
+    marginBottom: '8px',
+    [theme.breakpoints.down('md')]: {
+      fontSize: '22px',
+    },
+  },
+  '> p': {
+    fontWeight: 400,
+    fontSize: '14px',
+    lineHeight: '140%',
+    color: '#536166',
+    [theme.breakpoints.down('md')]: {
+      textAlign: 'center',
+    },
+  },
+}));
 
 const Banner = () => {
   const router = useRouter();
 
   return (
-    <Box sx={{ height: '60vh', zIndex: 0, position: 'relative' }}>
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          paddingTop: '80px',
-        }}
-      >
-        <Typography
-          sx={{
-            fontWeight: 700,
-            fontSize: '36px',
-            lineHeight: '140%',
-            color: '#536166',
-          }}
-        >
-          尋找夥伴
-        </Typography>
-        <Typography
-          sx={{
-            fontWeight: 400,
-            fontSize: '14px',
-            lineHeight: '140%',
-            color: '#536166',
-          }}
-        >
-          想找到一起交流的學習夥伴嗎
-        </Typography>
-        <Typography
-          sx={{
-            fontWeight: 400,
-            fontSize: '14px',
-            lineHeight: '140%',
-            color: '#536166',
-          }}
-        >
-          註冊加入會員，並填寫個人資料，你的資訊就會刊登在頁面上囉！
-        </Typography>
-        <Button
-          variant="contained"
-          sx={{
-            fontSize: '16px',
-            marginTop: '40px',
-            color: '#FFFFFF',
-            boxShadow: '0px 4px 10px rgba(89, 182, 178, 0.5)',
-            borderRadius: '20px',
-            padding: '9px 40px',
-          }}
-          onClick={() => {
-            router.push('/login');
-          }}
-        >
-          註冊找夥伴
-        </Button>
-      </Box>
-      <BannerImage />
-    </Box>
+    <StyledBanner>
+      <StyledContent>
+        <h1>尋找夥伴</h1>
+        <p>想找到一起交流的學習夥伴嗎</p>
+        <p>註冊加入會員，並填寫個人資料，你的資訊就會刊登在頁面上囉！</p>
+        <Button onClick={() => router.push('/login')}>註冊找夥伴</Button>
+      </StyledContent>
+      <picture>
+        <Image
+          src={partnerImg.src}
+          alt="尋找夥伴"
+          height="inherit"
+          background="linear-gradient(#fcfefe 10%, #e0f1f2 40%)"
+          borderRadius="0"
+        />
+      </picture>
+    </StyledBanner>
   );
 };
 
