c0cef2ec35ee2e81ff3d3a4d3cef5dbea13a1fb4
hsuifang <whalekiller03@gmail.com>
2023-12-04 22:00:52 +0800

    style(userTabs): create userTabs.styled

---
+++ components/Profile/UserTabs/UserTabs.styled.jsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import { Box } from '@mui/material';
+
+export const StyledTabContextBox = styled(Box)(({ theme }) => ({
+  borderBottom: '1px solid #536166',
+  color: theme.secondary, // Assuming secondary is a valid theme property
+  borderColor: theme.secondary, // Use borderColor for indicator color
+  '@media (max-width: 767px)': {
+    width: '100%',
+  },
+}));
+
+export const StyledPanelBox = styled(Box)`
+  width: 720px;
+  padding: 40px 30px;
+  margin-top: '10px';
+  @media (max-width: 767px) {
+    width: 100%;
+    padding: 30px;
+  }
+`;
+
+export const StyledPanelText = styled(Box)`
+  display: flex;
+  p {
+    color: #293a3d;
+    font-weight: 500;
+    white-space: nowrap;
+    min-width: 50px;
+  }
+  span {
+    color: #536166;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 140%;
+    margin-left: 12px;
+  }
+  @media (max-width: 767px) {
+    flex-direction: column;
+    span {
+      margin-left: 0px;
+    }
+  }
+`;
--- components/Profile/UserTabs/index.jsx
+++ components/Profile/UserTabs/index.jsx
@@ -1,55 +1,14 @@
 import { useState } from 'react';
-import styled from '@emotion/styled';
-import { Box, Typography, Divider, Skeleton } from '@mui/material';
+import { Box } from '@mui/material';
 import Tab from '@mui/material/Tab';
 import { TabContext } from '@mui/lab';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
-import { WANT_TO_DO_WITH_PARTNER } from '@/constants/member';
-import { mapToTable } from '@/utils/helper';
-
-const StyledTabContextBox = styled(Box)(({ theme }) => ({
-  borderBottom: '1px solid #536166',
-  color: theme.secondary, // Assuming secondary is a valid theme property
-  borderColor: theme.secondary, // Use borderColor for indicator color
-  '@media (max-width: 767px)': {
-    width: '100%',
-  },
-}));
-
-const StyledPanelBox = styled(Box)`
-  width: 720px;
-  padding: 40px 30px;
-  margin-top: '10px';
-  @media (max-width: 767px) {
-    width: 100%;
-    padding: 30px;
-  }
-`;
-
-const StyledPanelText = styled(Box)`
-  display: flex;
-  p {
-    color: #293a3d;
-    font-weight: 500;
-    white-space: nowrap;
-    min-width: 50px;
-  }
-  span {
-    color: #536166;
-    font-size: 14px;
-    font-style: normal;
-    font-weight: 400;
-    line-height: 140%;
-    margin-left: 12px;
-  }
-  @media (max-width: 767px) {
-    flex-direction: column;
-    span {
-      margin-left: 0px;
-    }
-  }
-`;
+import {
+  StyledTabContextBox,
+  StyledPanelBox,
+  StyledPanelText,
+} from './UserTabs.styled';
 
 const UserTabs = ({
   description = '',
@@ -57,7 +16,8 @@ const UserTabs = ({
   isLoading = false,
 }) => {
   const [value, setValue] = useState('1');
-
+  // TODO
+  console.log(description, wantToLearnList, isLoading);
   return (
     <Box
       sx={{
