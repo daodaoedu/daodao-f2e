ee36e72676afd852efb35c72e166c49867155137
Pei Wang <65725825+pepe1113@users.noreply.github.com>
2023-12-24 00:49:41 +0800

    Merge pull request #21 from pepe1113/feature/donate_link
    Add donate promotion bar

--- shared/components/Navigation_v2/index.jsx
+++ shared/components/Navigation_v2/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { AppBar } from '@mui/material';
 import MainNav from './MainNav';
+import PromotionBar from './PromotionBar';
 
 const NavigationWrapper = styled(AppBar)`
   display: flex;
@@ -15,17 +16,31 @@ const NavigationWrapper = styled(AppBar)`
   }
 `;
 
+const donateTexts = [
+  '✨島島阿學需要你的支持，讓人人都享有同等資源✨',
+  '✨推廣民主教育，島島阿學需要你的支持✨',
+  '✨用捐款與島島阿學一同推動民主教育✨',
+];
+
+const buildRandomText = () => {
+  const randomIndex = Math.floor(Math.random() * donateTexts.length);
+  return donateTexts[randomIndex];
+};
+
 // const ToolbarWrapper = styled(Toolbar)`
 //   margin: 0 auto;
 // `;
 // 問卷 https://docs.google.com/forms/d/e/1FAIpQLSeyU9-Q-kIWp5uutcik3h-RO4o5VuG6oG0m-4u1Ua18EOu3aw/viewform
 const Navigation = () => {
   return (
-    <NavigationWrapper position="sticky">
-      {/* <Toolbar> */}
-      <MainNav />
-      {/* </Toolbar> */}
-    </NavigationWrapper>
+    <>
+      <PromotionBar link="https://ocf.tw/p/daodao/" text={buildRandomText()} />
+      <NavigationWrapper position="sticky">
+        {/* <Toolbar> */}
+        <MainNav />
+        {/* </Toolbar> */}
+      </NavigationWrapper>
+    </>
   );
 };
 
