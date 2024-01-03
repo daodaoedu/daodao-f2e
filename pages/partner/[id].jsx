fff413ad118d3623d944268b97ce125de1bc7111
hsuifang <whalekiller03@gmail.com>
2024-01-03 22:51:48 +0800

    chore: test dynamic routing page

--- pages/partner/[id].jsx
+++ pages/partner/[id].jsx
@@ -1,8 +1,9 @@
+'use client';
+
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 import styled from '@emotion/styled';
-import { Box } from '@mui/material';
 import Navigation from '@/shared/components/Navigation_v2';
 import Footer from '@/shared/components/Footer_v2';
 import Profile from '@/components/Profile';
@@ -12,9 +13,23 @@ const HomePageWrapper = styled.div`
   --section-height-offset: 80px;
 `;
 
-const index = ({ user }) => {
+const index = () => {
   const router = useRouter();
   const { id } = router.query;
+  let user = {
+    name: '黃芊宇',
+    educationStepLabel: '大學',
+    tagList: [
+      '實驗教育實驗教育',
+      '標籤最多是八個字',
+      '前端工程師',
+      '後端工程師',
+      '太多標籤第二行',
+      '戶外運動',
+    ],
+    photoURL: 'https://i.imgur.com/QpMVwqe.png',
+    location: '台北市松山區',
+  };
 
   const fetchUser = async () => {
     console.log(id);
@@ -33,31 +48,3 @@ const index = ({ user }) => {
 };
 
 export default index;
-
-export async function getStaticPaths() {
-  return {
-    paths: [{ params: { id: '22' } }],
-    fallback: true,
-  };
-}
-
-export const getStaticProps = async () => {
-  return {
-    props: {
-      user: {
-        name: '黃芊宇',
-        educationStepLabel: '大學',
-        tagList: [
-          '實驗教育實驗教育',
-          '標籤最多是八個字',
-          '前端工程師',
-          '後端工程師',
-          '太多標籤第二行',
-          '戶外運動',
-        ],
-        photoURL: 'https://i.imgur.com/QpMVwqe.png',
-        location: '台北市松山區',
-      },
-    },
-  };
-};
