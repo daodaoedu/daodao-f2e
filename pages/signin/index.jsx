1de428776dadaf8e816e1b7a58a65ab476be062d
hsuifang <whalekiller03@gmail.com>
2024-01-16 01:17:09 +0800

    fix(signin): Delete SignInStyled and move styled in to index

--- pages/signin/index.jsx
+++ pages/signin/index.jsx
@@ -15,11 +15,49 @@ import Checkbox from '@mui/material/Checkbox';
 import SEOConfig from '@/shared/components/SEO';
 import Navigation from '@/shared/components/Navigation_v2';
 import Footer from '@/shared/components/Footer_v2';
-import {
-  HomePageWrapper,
-  StyledContentWrapper,
-  StyledQuestionInput,
-} from './Signin.styled';
+import styled from '@emotion/styled';
+
+export const HomePageWrapper = styled.div`
+  --section-height: calc(100vh - 80px);
+  --section-height-offset: 80px;
+  background: linear-gradient(0deg, #f3fcfc, #f3fcfc), #f7f8fa;
+`;
+
+export const StyledContentWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 16px;
+  margin: 60px auto;
+  max-width: 50%;
+  width: 100%;
+  @media (max-width: 767px) {
+    max-width: 80%;
+    .title {
+      text-overflow: ellipsis;
+      width: 100%;
+    }
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 22px;
+    line-height: 140%;
+    text-align: center;
+    color: #536166;
+    margin-top: 40px;
+  }
+`;
+
+export const StyledQuestionInput = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  margin-top: 20px;
+`;
 
 function EditPage() {
   const router = useRouter();
