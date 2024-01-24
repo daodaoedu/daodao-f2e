e7a39ee3feed077281b0f7de143c9a0a91b325f6
hsuifang <whalekiller03@gmail.com>
2024-01-24 21:48:52 +0800

    fix(profile): fix tags of profile

--- components/Partner/PartnerList/PartnerCard/PartnerCardTag.jsx
+++ components/Partner/PartnerList/PartnerCard/PartnerCardTag.jsx
@@ -4,16 +4,20 @@ const PartnerCardTag = ({ tagList = [] }) => {
   const showItems = tagList.slice(0, 5);
   const hideItems = tagList.slice(5);
   return (
-    <StyledTagContainer container gap="8px" mb="12px">
-      {showItems.map((tag) => (
-        <StyledTagText item key={tag} fontWeight="400">
-          {tag}
-        </StyledTagText>
-      ))}
-      {hideItems.length > 0 && (
-        <StyledTagText fontWeight="bold">{hideItems.length}</StyledTagText>
-      )}
-    </StyledTagContainer>
+    tagList.length > 0 && (
+      <StyledTagContainer container gap="8px" mb="12px">
+        {showItems
+          .filter((t) => typeof t === 'string' && t !== '')
+          .map((tag) => (
+            <StyledTagText item key={tag} fontWeight="400">
+              {tag}
+            </StyledTagText>
+          ))}
+        {hideItems.length > 0 && (
+          <StyledTagText fontWeight="bold">{hideItems.length}</StyledTagText>
+        )}
+      </StyledTagContainer>
+    )
   );
 };
 
