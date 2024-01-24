5e9b25af7c95878b2040498483acc7266b56d769
Johnson Mao <tutelary.maomao@gmail.com>
2024-01-24 23:01:50 +0800

    ✨ add fetch hook

--- hooks/useFetch.jsx
+++ hooks/useFetch.jsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from 'react';
 
-const useFetch = (url, initialValue) => {
-  const [result, setResult] = useState(initialValue);
-  const [loading, setLoading] = useState(true);
+const useFetch = (url, { initialValue } = {}) => {
+  const [data, setData] = useState(initialValue);
+  const [isFetching, setIsFetching] = useState(true);
+
   useEffect(() => {
-    fetch(url)
+    const abortController = new AbortController();
+
+    setIsFetching(true);
+
+    fetch(url, { signal: abortController.signal })
       .then((res) => res.json())
-      .then((json) => {
-        setResult(json);
-        setLoading(false);
-      });
-  }, []);
-  return { result, loading };
+      .then((json) => setData(json))
+      .finally(() => setIsFetching(false));
+
+    return () => abortController.abort();
+  }, [url]);
+
+  return { data, isFetching };
 };
 
 export default useFetch;
