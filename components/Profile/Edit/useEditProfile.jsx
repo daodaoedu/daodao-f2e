136156a510d8d111ac42d3c1331c839297c22bf4
hsuifang <whalekiller03@gmail.com>
2024-02-14 21:20:59 +0800

    refactor(profile/Edit): Add Zod in edit form

--- components/Profile/Edit/useEditProfile.jsx
+++ components/Profile/Edit/useEditProfile.jsx
@@ -1,7 +1,8 @@
 import dayjs from 'dayjs';
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { updateUser } from '@/redux/actions/user';
+import { z } from 'zod';
 
 const initialState = {
   name: '',
@@ -27,6 +28,53 @@ const initialState = {
   district: '',
 };
 
+const buildValidator = (maxLength, regex, maxMsg, regMsg) =>
+  z.string().max(maxLength, maxMsg).regex(regex, regMsg).optional();
+
+const tempSchema = Object.keys(initialState).reduce((acc, key) => {
+  return key !== 'birthDay'
+    ? {
+        ...acc,
+        [key]: z.string().optional(),
+      }
+    : acc;
+}, {});
+
+const schema = z.object({
+  ...tempSchema,
+  name: z
+    .string()
+    .min(1, { message: '請輸入名字' })
+    .max(50, { message: '名字過長' })
+    .optional(),
+  isOpenLocation: z.boolean().optional(),
+  isOpenProfile: z.boolean().optional(),
+  instagram: buildValidator(
+    30,
+    /^(|[a-zA-Z0-9_.]{2,20})$/,
+    '長度最多30個字元',
+    '長度最少2個字元，支援英文、數字、底線、句號',
+  ),
+  facebook: buildValidator(
+    64,
+    /^(|[a-zA-Z0-9_.]{5,20})$/,
+    '長度最多64個字元',
+    '長度最少5個字元，支援英文、數字、底線、句號',
+  ),
+  discord: buildValidator(
+    32,
+    /^(|[a-zA-Z0-9_.]{2,20})$/,
+    '長度最多32個字元',
+    '長度最少2個字元，支援英文、數字、底線、句號',
+  ),
+  line: buildValidator(
+    20,
+    /^(|[a-zA-Z0-9_.]{6,20})$/,
+    '長度最多20個字元',
+    '長度最少6個字元，支援英文、數字、底線、句號',
+  ),
+});
+
 const userReducer = (state, payload) => {
   const { key, value, isMultiple = false } = payload;
   if (isMultiple) {
@@ -47,13 +95,35 @@ const userReducer = (state, payload) => {
 
 const useEditProfile = () => {
   const reduxDispatch = useDispatch();
-
   const [userState, stateDispatch] = useReducer(userReducer, initialState);
+  const [errors, setErrors] = useState({});
 
-  // TODO ErrorMap
+  const validate = (state = {}, isPartial = false) => {
+    const [key, value] = Object.entries(state)[0];
+    if (key !== 'birthDay') {
+      const result = isPartial
+        ? schema.partial({ [key]: true }).safeParse({ [key]: value })
+        : schema.safeParse({ [key]: value });
+
+      if (!result.success) {
+        result.error.errors.forEach((err) => {
+          setErrors({ [err.path[0]]: err.message });
+        });
+      }
+      if (isPartial && result.success) {
+        const obj = { ...errors };
+        delete obj[key];
+        setErrors(obj);
+      }
+
+      return result.success;
+    }
+    return true;
+  };
 
   const onChangeHandler = ({ key, value, isMultiple }) => {
     stateDispatch({ key, value, isMultiple });
+    validate({ [key]: value }, true);
   };
 
   const onSubmit = async ({ id, email }) => {
@@ -105,10 +175,19 @@ const useEditProfile = () => {
     reduxDispatch(updateUser(payload));
   };
 
+  const checkBeforeSubmit = ({ id, email }) => {
+    if (validate(userState)) {
+      onSubmit({ id, email });
+      return true;
+    }
+    return false;
+  };
+
   return {
     userState,
     onChangeHandler,
-    onSubmit,
+    onSubmit: checkBeforeSubmit,
+    errors,
   };
 };
 
