074dd0c2311f4167d0ac1fc32be82c211f45ccaa
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2024-06-30 22:21:04 +0800

    fix: search group list (#57)
    * fix: search group list
    
    * feat: add api proxy for development mode

--- redux/sagas/groupSaga.js
+++ redux/sagas/groupSaga.js
@@ -1,4 +1,7 @@
 import { put, takeLatest, select } from 'redux-saga/effects';
+import { AREAS } from '@/constants/areas';
+import { CATEGORIES } from '@/constants/category';
+import { EDUCATION_STEP } from '@/constants/member';
 import req from '@/utils/request';
 
 import {
@@ -13,8 +16,34 @@ function* getGroupItems() {
   const {
     group: { pageSize, query },
   } = yield select();
-  const queryString = new URLSearchParams({ ...query, pageSize }).toString();
-  const URL = `${GROUP_API_URL}?${queryString}`;
+
+  const urlSearchParams = new URLSearchParams({ pageSize });
+  const searchParamsOptions = {
+    area: AREAS,
+    category: CATEGORIES,
+    partnerEducationStep: EDUCATION_STEP,
+    isGrouping: true,
+    q: true,
+  };
+
+  Object.keys(searchParamsOptions).forEach((key) => {
+    const searchParam = query[key];
+    const option = searchParamsOptions[key];
+
+    if (!searchParam || !option) return;
+
+    if (Array.isArray(option)) {
+      urlSearchParams.append(key, searchParam
+        .split(',')
+        .filter((item) => option.some((option) => option.label === item))
+        .join(','))
+    } else {
+      urlSearchParams.append(key, searchParam);
+    }
+  });
+
+  const URL = `${GROUP_API_URL}?${urlSearchParams.toString()}`;
+
   try {
     const response = yield req(URL);
     yield put(getGroupItemsSuccess(response));
