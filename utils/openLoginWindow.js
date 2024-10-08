c420a588f3f41546c27e52a308101de6bba9dd91
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2024-10-08 22:28:20 +0800

    feat: adjust sign-in page login flow (#93)

--- utils/openLoginWindow.js
+++ utils/openLoginWindow.js
@@ -1,15 +1,24 @@
 import { getRedirectionStorage } from './storage';
 
 export default function openLoginWindow(
-  redirection = '/',
+  redirection = '',
   target = '/login/popup',
 ) {
   const width = 520;
   const height = 760;
-  const left = (window.innerWidth - width) / 2;
-  const top = (window.innerHeight - height) / 2;
+  const left = (window.screen.width - width) / 2;
+  const top = (window.screen.height - height) / 2;
   const features = `left=${left},top=${top},width=${width},height=${height}`;
   const loginWindow = window.open('', '_blank', features);
-  getRedirectionStorage().set(redirection);
+  if (redirection) getRedirectionStorage().set(redirection);
   if (loginWindow) loginWindow.location = target;
+
+  return new Promise((resolve) => {
+    const loop = setInterval(() => {
+      if (loginWindow.closed) {
+        clearInterval(loop);
+        resolve();
+      }
+    }, 500);
+  });
 }
