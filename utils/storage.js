21222a6d62cb7e75d0d23e785c872ed3acb1bb00
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2024-11-03 12:15:24 +0800

    feat(shared): add text with links component

--- utils/storage.js
+++ utils/storage.js
@@ -16,4 +16,5 @@ export default function createStorage(key, storage = localStorage) {
   return { set, get, remove };
 }
 
-export const getRedirectionStorage = () => createStorage('_redirection');
+export const getRedirectionStorage = () => createStorage('_r');
+export const getTrustWebsitesStorage = () => createStorage('_trustWeb');
