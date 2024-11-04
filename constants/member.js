78bd81a9b9017d602917a7ba07c422e7701ace89
xiaoxu <innocent0705@gmail.com>
2024-11-05 00:41:34 +0800

    fix: update the option description

--- constants/member.js
+++ constants/member.js
@@ -46,7 +46,7 @@ export const ROLE = [
     image: 'https://i.imgur.com/cXZXfBL.png',
   },
   {
-    label: '終生學習',
+    label: '其他',
     key: 'other',
     value: 'other',
     image: 'https://i.imgur.com/Z7oGEnb.png',
@@ -100,7 +100,7 @@ export const EDUCATION_STEP = [
     value: 'doctor',
   },
   {
-    label: '終生學習',
+    label: '其他',
     key: 'other',
     value: 'other',
   },
