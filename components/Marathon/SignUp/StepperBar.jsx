616623cc909f8c501c541c12f278e0dc7220ac57
ruby10127130 <ruby10127130@gmail.com>
2024-12-28 15:07:21 +0800

    feat: adjust stepper bar sticky top position in marathon form

--- components/Marathon/SignUp/StepperBar.jsx
+++ components/Marathon/SignUp/StepperBar.jsx
@@ -16,7 +16,7 @@ export const StyledSaveBar = styled(Box)`
   box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12);
   position: sticky;
   z-index: 99;
-  top: 118px;
+  top: 105px;
   width: 100%;
   left: 0;
 
