07992d18afba6c5e34175fd5ea15f7855ad3e2d0
xiaoxu <innocent0705@gmail.com>
2025-01-09 01:18:55 +0800

    fix:update marathon content

--- components/Marathon/Faq/index.jsx
+++ components/Marathon/Faq/index.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect, useRef } from 'react';
+import Link from 'next/link';
 import styled from "@emotion/styled";
 import {
   Box,
 } from "@mui/material";
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { cn } from '@/utils/cn';
 
 const StyledGroup = styled(Box)`
   width: 100%;
@@ -216,6 +218,26 @@ export default function Faq() {
             </li>
           </ol>
         </StyledList>
+      </Accordion>
+      <Accordion
+        title="我想團報但找不到人？"
+      >
+          歡迎填寫團報表單，我們將協助促成！
+        <Link href="https://forms.gle/BZ24JnTxid4y7CCV6"
+              className={cn("block text-base font-medium p-2.5 rounded-lg text-basic-400 transition-colors duration-300")}
+        >
+          https://forms.gle/BZ24JnTxid4y7CCV6
+        </Link>
+      </Accordion>
+      <Accordion
+        title="有哪些機會可以獲得課程費用優惠折抵呢？"
+      >
+          參加說明會、轉分享貼文等方式都可獲得優惠，優惠活動請參考折價券申請表單：
+        <Link href="https://forms.gle/BZ24JnTxid4y7CCV6"
+              className={cn("block text-base font-medium p-2.5 rounded-lg text-basic-400 transition-colors duration-300")}
+        >
+          https://forms.gle/9Pfa9Q5d27m1JEpUA （折抵金額無上限）
+        </Link>
       </Accordion>
     </StyledGroup>
   );
