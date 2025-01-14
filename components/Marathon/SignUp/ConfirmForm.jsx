daf99843eea1e85bef97a1abbd31baaeac084e69
ruby10127130 <ruby10127130@gmail.com>
2025-01-14 20:34:39 +0800

    feat: redirect to /learning-marathon/profile?id=[marathon.id] after user update marathon data in page /learning-marathon/signup

--- components/Marathon/SignUp/ConfirmForm.jsx
+++ components/Marathon/SignUp/ConfirmForm.jsx
@@ -293,7 +293,7 @@ export default function ConfirmForm({
     switch (marathonState.apiStateWithType) {
       case 'updateMarathonProfileSuccess': {
         toast.success('更新成功');
-        router.push('/profile?id=my-marathon');
+        router.push(`/learning-marathon/profile?id=${marathonState._id}`);
         break;
       }
       case 'createMarathonProfileByTokenSuccess': {
