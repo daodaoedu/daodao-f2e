64c8af143e2e9741fd371cec70c62d690a9077ef
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-20 22:37:48 +0800

    refactor(manage): layout

---
+++ layout/ManageLayout.tsx
@@ -0,0 +1,82 @@
+import { usePathname } from 'next/navigation';
+import Sidebar from '@/shared/components/Sidebar';
+import Collapse from '@/shared/components/Collapse';
+import DefaultLayout from './DefaultLayout';
+
+export default function ManageLayout({ children }: React.PropsWithChildren) {
+  const pathname = usePathname();
+
+  return (
+    <DefaultLayout>
+      <div className="bg-primary-palest">
+        <div className="flex p-4 pt-12 gap-10 max-w-6xl mx-auto min-h-screen-with-padding-top">
+          <div className="basis-80">
+            <Sidebar>
+              <Sidebar.Link href="/manage" isActive={pathname === '/manage'}>
+                我的小島
+              </Sidebar.Link>
+              <Sidebar.Link
+                href="/manage/project"
+                isActive={pathname === '/manage/project'}
+                isDisabled
+              >
+                我的學習計畫
+              </Sidebar.Link>
+              <Sidebar.Link
+                href="/manage/intro"
+                isActive={pathname === '/manage/intro'}
+                isDisabled
+              >
+                個人名片
+              </Sidebar.Link>
+              <Collapse>
+                <Sidebar.Button className="w-full p-0">
+                  <Collapse.Toggle className="w-full px-10 py-2" withIcon>
+                    百寶箱
+                  </Collapse.Toggle>
+                </Sidebar.Button>
+                <Collapse.List className="*:my-2 *:aria-hidden:my-0">
+                  <Collapse.Item>
+                    <Sidebar.Link
+                      className="pl-14"
+                      href="/manage/treasure"
+                      isDisabled
+                    >
+                      我的收藏
+                    </Sidebar.Link>
+                  </Collapse.Item>
+                  <Collapse.Item>
+                    <Sidebar.Link
+                      className="pl-14"
+                      href="/manage/treasure"
+                      isDisabled
+                    >
+                      我的足跡
+                    </Sidebar.Link>
+                  </Collapse.Item>
+                  <Collapse.Item>
+                    <Sidebar.Link
+                      className="pl-14"
+                      href="/manage/treasure"
+                      isDisabled
+                    >
+                      追蹤的夥伴
+                    </Sidebar.Link>
+                  </Collapse.Item>
+                </Collapse.List>
+              </Collapse>
+              <Sidebar.Link
+                href="/manage/classroom"
+                isActive={pathname === '/manage/classroom'}
+                isDisabled
+              >
+                我的教室
+              </Sidebar.Link>
+            </Sidebar>
+          </div>
+          <div className="flex-1">{children}</div>
+        </div>
+      </div>
+    </DefaultLayout>
+  );
+}
