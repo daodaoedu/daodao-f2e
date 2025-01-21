011c47a7115abcc5db3cc8b6cfe8ad1ac4fd70cc
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-21 22:58:08 +0800

    refactor(shared): dropdown toggle use button component

--- shared/components/Dropdown.tsx
+++ shared/components/Dropdown.tsx
@@ -1,13 +1,14 @@
-import { useEffect, useRef } from "react";
-import { ToggleProvider, useToggle } from "@/contexts/Toggle";
-import { cn } from "@/utils/cn";
+import { useEffect, useRef } from 'react';
+import { ToggleProvider, useToggle } from '@/contexts/Toggle';
+import { cn } from '@/utils/cn';
+import Button, { ButtonProps } from './Button';
 
 interface DropdownProps {
   as?: React.ElementType;
   children: React.ReactNode;
 }
 
-function DropdownContent({ as: Component = "div", children }: DropdownProps) {
+function DropdownContent({ as: Component = 'div', children }: DropdownProps) {
   const { isOpen, setIsOpen } = useToggle();
   const ref = useRef<HTMLElement>(null);
 
@@ -22,10 +23,10 @@ function DropdownContent({ as: Component = "div", children }: DropdownProps) {
       setIsOpen(false);
     };
     if (isOpen) {
-      window.addEventListener("click", handleClick);
+      window.addEventListener('click', handleClick);
     }
     return () => {
-      window.removeEventListener("click", handleClick);
+      window.removeEventListener('click', handleClick);
     };
   }, [isOpen]);
 
@@ -44,36 +45,47 @@ function Dropdown({ as, children }: DropdownProps) {
   );
 }
 
-interface DropdownToggleProps {
+interface DropdownToggleProps extends ButtonProps {
   className?: string;
   children: React.ReactNode;
   withIcon?: boolean;
 }
 
-function Toggle({ className, children, withIcon }: DropdownToggleProps) {
+function Toggle({
+  className,
+  children,
+  withIcon,
+  onClick,
+  ...props
+}: DropdownToggleProps) {
   const { isOpen, setIsOpen } = useToggle({
-    errorMessage: "Dropdown.Toggle must be used within an Dropdown",
+    errorMessage: 'Dropdown.Toggle must be used within an Dropdown',
   });
 
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    setIsOpen(!isOpen);
+    onClick?.(e);
+  };
+
   return (
-    <button
-      type="button"
-      className={cn("flex items-center", className)}
+    <Button
+      className={cn('flex items-center', withIcon && 'pl-6 pr-4', className)}
       aria-pressed={isOpen}
-      onClick={() => setIsOpen(!isOpen)}
+      onClick={handleClick}
+      {...props}
     >
       {children}
       {withIcon && (
         <div
           className={cn(
-            "transition-transform p-2",
-            isOpen ? "-rotate-180" : "rotate-0"
+            'transition-transform p-2',
+            isOpen ? '-rotate-180' : 'rotate-0'
           )}
         >
           <div className="w-2 h-2 rotate-45 -translate-y-0.5 border-b-2 border-r-2 border-solid border-current" />
         </div>
       )}
-    </button>
+    </Button>
   );
 }
 
@@ -84,15 +96,15 @@ interface DropdownListProps {
 
 function List({ children, className }: DropdownListProps) {
   const { isOpen } = useToggle({
-    errorMessage: "Dropdown.List must be used within an Dropdown",
+    errorMessage: 'Dropdown.List must be used within an Dropdown',
   });
 
   return (
     <ul
       className={cn(
-        "group absolute p-2 rounded-lg shadow-lg bg-white transition-[transform,opacity] origin-top",
+        'group absolute p-2 rounded-lg shadow-lg bg-white transition-[transform,opacity] origin-top',
         className,
-        isOpen ? "opacity-100 scale-y-100" : "opacity-30 scale-y-0"
+        isOpen ? 'opacity-100 scale-y-100' : 'opacity-30 scale-y-0'
       )}
       aria-hidden={!isOpen}
     >
