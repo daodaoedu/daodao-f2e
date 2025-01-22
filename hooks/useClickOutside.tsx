32ae556b6f9b629acab1a365d882bac5889f5a07
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-22 23:35:44 +0800

    refactor(shared): dropdown and add click outside hook

---
+++ hooks/useClickOutside.tsx
@@ -0,0 +1,35 @@
+import { useCallback, useEffect, useRef } from 'react';
+
+interface UseClickOutsideProps {
+  setState: (state: boolean) => void;
+}
+
+export default function useClickOutside<T extends HTMLElement>({
+  setState,
+}: UseClickOutsideProps) {
+  const ref = useRef<T | null>(null);
+
+  const setRef = useCallback((node: T) => {
+    ref.current = node;
+  }, []);
+
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        ref.current?.contains?.(e.target)
+      ) {
+        return;
+      }
+      setState(false);
+    };
+
+    window.addEventListener('click', handleClick);
+
+    return () => {
+      window.removeEventListener('click', handleClick);
+    };
+  }, [setState]);
+
+  return { ref, setRef };
+}
