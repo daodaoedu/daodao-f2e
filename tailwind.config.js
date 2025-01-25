edbf0b6d0d427a65ad2da56984c27c74d065d890
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-25 14:32:17 +0800

    feat(shared): add button animation for UX

--- tailwind.config.js
+++ tailwind.config.js
@@ -150,6 +150,21 @@ module.exports = {
             },
           },
         },
+        [`.animate-button-ripple`]: {
+          animation:
+            'button-ripple var(--animation-duration, 500ms) var(--animation-delay, 0ms) forwards cubic-bezier(0, 0, 0.2, 1)',
+          '@keyframes button-ripple': {
+            '0%': { transform: 'translate(-50%, -50%) scale(0)', opacity: 1 },
+            '75%': {
+              transform: 'translate(-50%, -50%) scale(4)',
+              opacity: 0.2,
+            },
+            '100%': {
+              transform: 'translate(-50%, -50%) scale(6)',
+              opacity: 0,
+            },
+          },
+        },
       });
       addUtilities({
         [`.animate-slide-x-in`]: {
