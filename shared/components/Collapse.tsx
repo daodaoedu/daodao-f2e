a1229c5b0a073520467c9163966557de646c2d9e
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-25 22:59:25 +0800

    refactor(shared): rename toggle state name and return type

--- shared/components/Collapse.tsx
+++ shared/components/Collapse.tsx
@@ -1,12 +1,12 @@
-import { ToggleProvider, useToggle } from "@/contexts/Toggle";
-import { cn } from "@/utils/cn";
+import { ToggleProvider, useToggle } from '@/contexts/Toggle';
+import { cn } from '@/utils/cn';
 
 interface CollapseProps {
   as?: React.ElementType;
   children: React.ReactNode;
 }
 
-function Collapse({ as: Root = "div", children }: CollapseProps) {
+function Collapse({ as: Root = 'div', children }: CollapseProps) {
   return (
     <ToggleProvider>
       <Root className="relative">{children}</Root>
@@ -21,14 +21,14 @@ interface CollapseToggleProps {
 }
 
 function Toggle({ children, className, withIcon }: CollapseToggleProps) {
-  const { isOpen, setIsOpen } = useToggle({
-    errorMessage: "Collapse.Toggle must be used within a Collapse",
+  const [isOpen, setIsOpen] = useToggle({
+    errorMessage: 'Collapse.Toggle must be used within a Collapse',
   });
 
   return (
     <button
       type="button"
-      className={cn("flex items-center", className)}
+      className={cn('flex items-center', className)}
       aria-pressed={isOpen}
       onClick={() => setIsOpen(!isOpen)}
     >
@@ -36,8 +36,8 @@ function Toggle({ children, className, withIcon }: CollapseToggleProps) {
       {withIcon && (
         <div
           className={cn(
-            "transition-transform p-2",
-            isOpen ? "-rotate-180" : "rotate-0"
+            'transition-transform p-2',
+            isOpen ? '-rotate-180' : 'rotate-0'
           )}
         >
           <div className="w-2 h-2 rotate-45 -translate-y-0.5 border-b-2 border-r-2 border-solid border-current" />
@@ -53,18 +53,18 @@ interface CollapseListProps {
 }
 
 function List({ children, className }: CollapseListProps) {
-  const { isOpen } = useToggle({
-    errorMessage: "Collapse.List must be used within a Collapse",
+  const [isOpen] = useToggle({
+    errorMessage: 'Collapse.List must be used within a Collapse',
   });
 
   return (
     <ul
       className={cn(
-        "group transition-opacity",
-        "*:grid *:transition-[grid-template-rows]",
-        "*:grid-rows-[1fr] *:aria-hidden:grid-rows-[0fr] [&>*>*]:overflow-hidden",
+        'group transition-opacity',
+        '*:grid *:transition-[grid-template-rows]',
+        '*:grid-rows-[1fr] *:aria-hidden:grid-rows-[0fr] [&>*>*]:overflow-hidden',
         className,
-        isOpen ? "opacity-100" : "opacity-0"
+        isOpen ? 'opacity-100' : 'opacity-0'
       )}
       aria-hidden={!isOpen}
     >
@@ -81,7 +81,7 @@ interface CollapseItemProps {
 function Item({ children, className }: CollapseItemProps) {
   return (
     <li>
-      <div className={cn("overflow-hidden", className)}>{children}</div>
+      <div className={cn('overflow-hidden', className)}>{children}</div>
     </li>
   );
 }
