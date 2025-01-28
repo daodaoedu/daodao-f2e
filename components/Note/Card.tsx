72cf8d8e55c571f495ddafccc6f9ad6983ee72df
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-28 15:12:20 +0800

    feat(note): add project notes page

---
+++ components/Note/Card.tsx
@@ -0,0 +1,32 @@
+import Image from '@/shared/components/Image';
+import PostCard from '@/shared/components/Post/PostCard';
+
+interface NoteCardProps {
+  className?: string;
+}
+
+function NoteCard({ className }: NoteCardProps) {
+  return (
+    <PostCard className={className}>
+      <PostCard.Header
+        title="學習計畫一"
+        subtitle="第五週"
+        tag="便利貼"
+        date="2024/12/11"
+        viewCount={100}
+        isLocked={false}
+      />
+      <div className="mb-3 body-sm text-basic-500">
+        <p className="mb-3">
+          因為對剪影片和當 Youtuber 有興趣，我預計會研究搞笑型 Youtuber
+          的影片腳本與剪輯方式、拍攝我日常生活及練習剪輯，並建立 Youtube
+          頻道上傳影片。希望能藉此了解如何當一位 Youtuber。
+        </p>
+        <Image src="" alt="" height="300px" />
+      </div>
+      <PostCard.MoreActions />
+    </PostCard>
+  );
+}
+
+export default NoteCard;
