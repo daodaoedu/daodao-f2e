55ff99cb52c41843998183fee4aed71aa85226a8
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-01-28 21:47:27 +0800

    feat(note): add create modal and detail pages

---
+++ pages/manage/project/notes/index.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+import NoteCard from '@/components/Note/Card';
+import ProjectLayout from '@/layout/ProjectLayout';
+import Button from '@/shared/components/Button';
+import NoteCreate from '@/components/Note/Create';
+
+const NotesPage = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <>
+      <div className="mb-6 flex items-center justify-between body-md">
+        <div className="text-basic-500">便利貼 (2)</div>
+        <Button variant="solid" color="primary" onClick={() => setIsOpen(true)}>
+          新增便利貼
+        </Button>
+      </div>
+      <ul className="px-4 bg-basic-white flex flex-col rounded-2xl">
+        <li className="py-6 border-b last:border-b-0 border-solid border-basic-200">
+          <NoteCard
+            className="p-3 transition-shadow hover:shadow-basic-200/40 hover:shadow-lg"
+            detailLink={`/manage/project/notes/detail?id=${1}`}
+          />
+        </li>
+        <li className="py-6 border-b last:border-b-0 border-solid border-basic-200">
+          <NoteCard
+            className="p-3 transition-shadow hover:shadow-basic-200/40 hover:shadow-lg"
+            detailLink={`/manage/project/notes/detail?id=${2}`}
+          />
+        </li>
+      </ul>
+      <NoteCreate isOpen={isOpen} onClose={() => setIsOpen(false)} />
+    </>
+  );
+};
+
+NotesPage.getLayout = ProjectLayout;
+
+export default NotesPage;
