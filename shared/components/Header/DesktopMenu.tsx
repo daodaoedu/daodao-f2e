c2c63fa4af227d0f5aca48209e396c0f5fda567c
VincentXu <90778107+vincentxuu@users.noreply.github.com>
2025-02-19 17:09:04 +0800

    fix:Update DesktopMenu.tsx
    補上型別

--- shared/components/Header/DesktopMenu.tsx
+++ shared/components/Header/DesktopMenu.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import { MARATHON_LINKS, NAV_LINK, USER_LINK } from '@/constants/category';
+import { MARATHON_LINKS, NAV_LINK, USER_LINK, MenuItem } from '@/constants/category';
 import { useAuth, useAuthDispatch } from '@/contexts/Auth';
 import { cn } from '@/utils/cn';
 import Dropdown from '../Dropdown';
@@ -12,7 +12,7 @@ function DesktopMenu() {
     <>
       <nav>
         <ul className="flex items-center gap-1">
-          {NAV_LINK.map(({ link, name, target }) => (
+          {NAV_LINK.map(({ link, name, target }: MenuItem) => (
             <li key={name}>
               <Link
                 href={link}
