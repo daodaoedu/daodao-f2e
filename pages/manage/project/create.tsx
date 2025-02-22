28f23dc8cec4626cd0c85583da68f45fd1ba409b
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-02-23 00:00:51 +0800

    refactor: simplify project creation page SEO

--- pages/manage/project/create.tsx
+++ pages/manage/project/create.tsx
@@ -15,32 +15,12 @@ const ProjectPage = () => {
 
   const SEOData = useMemo(
     () => ({
-      title: '島島盃 - 2025 春季學習馬拉松｜多元學習資源平台｜島島阿學',
-      description:
-        '「島島阿學」盼能透過建立多元的學習資源網絡，讓自主學習者能找到合適的成長方法，進一步成為自己想成為的人，從中培養共好精神。目前正積極打造「可共編的學習資源平台」。',
+      title: '新增計畫｜多元學習資源平台｜島島阿學',
       keywords: '島島阿學',
       author: '島島阿學',
       copyright: '島島阿學',
       imgLink: 'https://www.daoedu.tw/preview.webp',
       link: `${process.env.HOSTNAME}${router?.asPath}`,
-      structuredData: [
-        {
-          '@context': 'https://schema.org',
-          '@type': 'WebSite',
-          url: 'https://www.daoedu.tw',
-          potentialAction: {
-            '@type': 'SearchAction',
-            'query-input': 'required name=q',
-            target: 'https://www.daoedu.tw/search?q={q}',
-          },
-        },
-        {
-          '@context': 'https://schema.org',
-          '@type': 'Organization',
-          url: 'https://www.daoedu.tw',
-          logo: 'https://www.daoedu.tw/favicon-112.png',
-        },
-      ],
     }),
     [router?.asPath]
   );
