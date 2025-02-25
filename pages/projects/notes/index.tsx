64fa6fc14e35f2defee4c8db9ed33c6f778688dd
xiaoxu <innocent0705@gmail.com>
2025-02-26 02:24:41 +0800

    feat: add project notes pages and enable notes navigation

---
+++ pages/projects/notes/index.tsx
@@ -0,0 +1,42 @@
+import { useSearchParams } from 'next/navigation';
+import NoteCard from '@/components/Note/Card';
+import getPublicProjectLayout from '@/layout/PublicProjectLayout';
+import {
+  useProjectNoteList,
+} from '@/hooks/api/project';
+
+const NotesPage = () => {
+  const searchParams = useSearchParams();
+  const projectId = searchParams.get('projectId') ?? undefined;
+
+  const {
+    data: notes,
+  } = useProjectNoteList(projectId);
+
+  if (!projectId) {
+    return <div>專案不存在</div>;
+  }
+
+  return (
+    <>
+      <ul className="px-4 bg-basic-white flex flex-col rounded-2xl">
+        {notes?.map((note) => (
+          <li
+            key={note.id}
+            className="py-6 border-b last:border-b-0 border-solid border-basic-200"
+          >
+            <NoteCard
+              data={note}
+              className="p-3 transition-shadow hover:shadow-basic-200/40 hover:shadow-lg"
+              detailLink={`/projects/notes/detail?id=${projectId}&noteId=${note.id}`}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+NotesPage.getLayout = getPublicProjectLayout;
+
+export default NotesPage;
