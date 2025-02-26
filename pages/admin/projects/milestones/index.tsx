b51ecf475249960f7ba0fe09aa44b11fe4ced43d
xiaoxu <innocent0705@gmail.com>
2025-02-27 00:08:38 +0800

    feat: Add admin project pages for project management and navigation

---
+++ pages/admin/projects/milestones/index.tsx
@@ -0,0 +1,63 @@
+import getPublicProjectLayout from '@/layout/PublicProjectLayout';
+import { useSearchParams } from 'next/navigation';
+import { Skeleton } from '@mui/material';
+import useProjectMilestoneList from '@/hooks/api/project/useProjectMilestoneList';
+import EmptyList from '@/components/Projects/ProjectList/EmptyList';
+import MilestoneItemView from '@/components/Milestones/MilestoneItemView';
+import { MilestonesProvider } from '@/contexts/Milestones';
+
+const ProjectMilestonesPage = () => {
+  const searchParams = useSearchParams();
+  const projectId = searchParams.get('id') ?? undefined;
+  const {
+    data: milestones,
+    isLoading,
+  } = useProjectMilestoneList(projectId);
+
+  return (
+    <div className="w-[750px] max-w-full mx-auto">
+      {isLoading ? (
+        <>
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height={120}
+            animation="wave"
+            className="mb-3"
+          />
+          <Skeleton
+            variant="rectangular"
+            width="100%"
+            height={300}
+            animation="wave"
+          />
+        </>
+      ) : (
+        <div>
+          {projectId && Array.isArray(milestones) && milestones.length > 0 ? (
+            milestones
+              .sort((a, b) => {
+                return a.week - b.week;
+              })
+              .map((milestone) => {
+                return (
+                  <MilestoneItemView
+                    milestone={milestone}
+                    key={milestone.id}
+                  />
+                );
+              })
+          ) : (
+            <EmptyList />
+          )}
+        </div>
+      )
+    }
+    </div>
+  );
+};
+ProjectMilestonesPage.getLayout = (page: React.ReactElement) =>
+  getPublicProjectLayout(
+    <MilestonesProvider>{page}</MilestonesProvider>
+  );
+export default ProjectMilestonesPage;
