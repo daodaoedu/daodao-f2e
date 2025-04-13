0f08991b8ac1124f679a094636aa597307f8ac34
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-04-13 15:09:12 +0800

    feat(modal): improve animation and add responsive drawer behavior

--- shared/components/Modal.tsx
+++ shared/components/Modal.tsx
@@ -50,24 +50,75 @@ function Modal({
   };
 
   useEffect(() => {
+    const bodyClassNames = [
+      'fixed',
+      'inset-x-0',
+      'min-h-svh',
+      'overflow-y-hidden',
+      'pointer-events-none',
+      'bg-black/30',
+    ];
+    const rootClassNames = [
+      'scale-95',
+      '-translate-y-[1.5%]',
+      'rounded-lg',
+      'overflow-hidden',
+    ];
+    const { scrollY } = window;
+    const rootElement = document.getElementById('__next');
     let timer: NodeJS.Timeout;
 
+    const checkIsDrawer = () => {
+      const windowWidth = window.innerWidth;
+
+      switch (size) {
+        case ModalSize.Small:
+          return windowWidth < 640;
+        case ModalSize.Medium:
+          return windowWidth < 768;
+        case ModalSize.Large:
+          return windowWidth < 1024;
+        default:
+          return false;
+      }
+    };
+
     if (isOpen) {
-      document.body.classList.add('overflow-y-hidden');
+      document.body.style.setProperty('top', `${scrollY * -1}px`);
+      document.body.classList.add(...bodyClassNames);
+      if (checkIsDrawer()) {
+        rootElement?.classList.add(
+          'transition-[transform,border-radius]',
+          'duration-500',
+          ...rootClassNames
+        );
+      }
       setRemoveDOM(false);
       setIsInitialized(true);
     } else {
       timer = setTimeout(() => {
         setRemoveDOM(true);
         onRemovedDOM?.();
-      }, 200);
+      }, 500);
     }
 
     return () => {
-      document.body.classList.remove('overflow-y-hidden');
+      document.body.style.removeProperty('top');
+      document.body.classList.remove(...bodyClassNames);
+      rootElement?.classList.remove(...rootClassNames);
+
+      /**
+       * 關閉彈窗後，恢復滾動位置
+       */
+      if (isOpen) {
+        document.documentElement.scrollTo({
+          top: scrollY,
+          behavior: 'instant',
+        });
+      }
       clearTimeout(timer);
     };
-  }, [isOpen, onRemovedDOM]);
+  }, [isOpen, size]);
 
   useEffect(() => {
     const handleWindowKeyUp = (e: KeyboardEvent) => {
@@ -108,33 +159,33 @@ function Modal({
   return (
     (!removeDOM || keepMounted) && (
       <Portal rootId={modalId}>
-        <button
-          type="button"
-          className={cn(
-            'fixed inset-0 z-[98] pointer-events-none border-none cursor-default',
-            isInitialized && [
-              isOpen
-                ? 'pointer-events-auto animate-fade-in bg-black/50'
-                : 'animate-fade-out',
-            ]
-          )}
-          onClick={hasCloseButton ? undefined : onClose}
-          onKeyUp={hasCloseButton ? undefined : handleKeyUp}
-          tabIndex={hasCloseButton ? -1 : undefined}
-        />
         <div
           className={cn(
             'fixed inset-0 z-[99] flex overflow-y-auto pointer-events-none',
-            'transition-opacity opacity-0 ease-in duration-200',
-            isInitialized && [isOpen ? 'animate-fade-in' : 'animate-fade-out']
+            isInitialized && isOpen && 'pointer-events-auto'
           )}
         >
+          <button
+            type="button"
+            className={cn(
+              'fixed inset-0 pointer-events-none border-none cursor-default',
+              'bg-black/60 duration-500',
+              isInitialized && [
+                isOpen
+                  ? 'pointer-events-auto animate-fade-in'
+                  : 'animate-fade-out',
+              ]
+            )}
+            onClick={hasCloseButton ? undefined : onClose}
+            onKeyUp={hasCloseButton ? undefined : handleKeyUp}
+            tabIndex={hasCloseButton ? -1 : undefined}
+          />
           <dialog
             ref={setDialogElement}
             open={!removeDOM}
             className={cn(
-              'relative top-[var(--dialog-top)] my-0 p-5 w-full',
-              'bg-white rounded-lg pointer-events-auto',
+              'relative top-[var(--dialog-top)] my-0 p-5 w-full rounded-lg bg-white',
+              'pointer-events-auto animate-distance-100dvh duration-500',
               size === ModalSize.Small &&
                 'sm:relative sm:top-12 sm:m-auto sm:p-10 sm:max-w-96',
               size === ModalSize.Medium &&
