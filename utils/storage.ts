fc567954a981bdb8dc44adcfa32fcdb4098faf18
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-04-26 21:40:03 +0800

    feat: add localStorage util for milestone drag reminder

--- utils/storage.ts
+++ utils/storage.ts
@@ -16,9 +16,26 @@ export default function createStorage<T>(key: string, storage = localStorage) {
   return { set, get, remove };
 }
 
+/** 獲取用於存儲用戶令牌的 localStorage */
 export const getTokenStorage = () => createStorage<string>('_token');
+
+/** 獲取用於存儲重定向URL的 localStorage */
 export const getRedirectionStorage = () => createStorage<string>('_r');
-export const getTrustWebsitesStorage = () => createStorage<string[]>('_trustWeb');
+
+/** 獲取用於存儲外連結受信任網站列表的 localStorage */
+export const getTrustWebsitesStorage = () =>
+  createStorage<string[]>('_trustWeb');
+
+/** 獲取用於存儲提醒填寫完整資料的 localStorage */
 export const getReminderStorage = () => createStorage<number>('_reminder');
-export const getMarathonErrorsStorage = () => createStorage('_marathonFormErrors');
-export const getDevOriginStorage = () => createStorage('_devOrigin');
+
+/** 獲取用於存儲馬拉松表單錯誤的 localStorage */
+export const getMarathonErrorsStorage = () =>
+  createStorage('_marathonFormErrors');
+
+/** 獲取用於存儲開發環境來源地址的 localStorage */
+export const getDevOriginStorage = () => createStorage<string>('_devOrigin');
+
+/** 獲取用於存儲是否提醒用戶里程碑拖拽會改變日期的 localStorage */
+export const getIsCheckDragMilestoneStorage = () =>
+  createStorage<boolean>('_isCheckDragMilestone');
