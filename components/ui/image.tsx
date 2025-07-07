4823af703ae2bb35dfeb3e43b5995e7d6dc7d1cf
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-07-07 22:55:59 +0800

    feat(ui): update badge and image components

--- components/ui/image.tsx
+++ components/ui/image.tsx
@@ -1,7 +1,15 @@
 import NextImage, { ImageProps as NextImageProps } from "next/image";
+import emptyCoverPng from "@/public/assets/empty-cover.png";
 
 export type ImageProps = NextImageProps;
 
-export function Image({ src, alt, ...props }: ImageProps) {
-  return <NextImage src={src} alt={alt} {...props} />;
+export function Image({ src, alt, onError, ...props }: ImageProps) {
+  const handleError = (
+    event: React.SyntheticEvent<HTMLImageElement, Event>
+  ) => {
+    const target = event.currentTarget as HTMLImageElement;
+    target.src = emptyCoverPng.src;
+    onError?.(event);
+  };
+  return <NextImage src={src} alt={alt} {...props} onError={handleError} />;
 }
