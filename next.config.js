aa1946ba55a8454a4f6ab2bb80c62f847e0a1219
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-07-11 23:19:36 +0800

    chore(config): update image domain and TS settings

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
-const isDev = process.env.NODE_ENV === 'development';
-const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? '';
+const isDev = process.env.NODE_ENV === "development";
+const apiUrl = process.env.NEXT_PUBLIC_API_URL ?? "";
 
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   buildExcludes: [
     /build-manifest\.json$/,
     /react-loadable-manifest\.json$/,
@@ -15,34 +15,39 @@ const withPWA = require('next-pwa')({
 const config = {
   reactStrictMode: false,
   staticPageGenerationTimeout: 600,
-  transpilePackages: ['@mdxeditor/editor'],
-  // 確保頁面跳轉時自動滾動到頂部
+  transpilePackages: ["@mdxeditor/editor"],
   experimental: {
     scrollRestoration: true,
   },
   images: {
-    domains: ['imgur.com', 'i.imgur.com', 'images.unsplash.com', 'lh3.googleusercontent.com'],
+    domains: [
+      "imgur.com",
+      "i.imgur.com",
+      "images.unsplash.com",
+      "lh3.googleusercontent.com",
+      "image.daoedu.tw",
+    ],
   },
   webpack: (config, options) => {
     const experiments = { ...config.experiments, topLevelAwait: true };
 
     config.module.rules.push({
       test: /\.svg$/,
-      use: [options.defaultLoaders.babel, '@svgr/webpack'],
+      use: [options.defaultLoaders.babel, "@svgr/webpack"],
     });
 
     return Object.assign(config, { experiments });
   },
   env: {
-    HOSTNAME: 'https://www.daoedu.tw',
-    NEXT_PUBLIC_DEV_URL: 'https://dev.daodao-notion-test.pages.dev',
+    HOSTNAME: "https://www.daoedu.tw",
+    NEXT_PUBLIC_DEV_URL: "https://dev.daodao-notion-test.pages.dev",
   },
   ...(isDev
     ? {
         async rewrites() {
           return [
             {
-              source: '/dev-proxy-api/:path*',
+              source: "/dev-proxy-api/:path*",
               destination: `${apiUrl}/:path*`,
             },
           ];
