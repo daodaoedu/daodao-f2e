83fb327600ecc2c4de9628a0b0527654af031094
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-07-12 18:38:31 +0800

    feat(email): add user contact feature

---
+++ features/email/hooks.tsx
@@ -0,0 +1,46 @@
+import useSWRMutation from "swr/mutation";
+import contractDoneImg from "@/public/assets/contactdone.png";
+import contractErrorImg from "@/public/assets/contacterror.png";
+import { Image } from "@/components/ui/image";
+import { emailAPI, getEmailPathname, SendEmailSchema } from "@/services/emails";
+import { useDialog } from "@/contexts/Dialog";
+
+interface SendEmailProps {
+  onSuccess?: () => void;
+}
+
+export const useSendEmail = ({ onSuccess }: SendEmailProps = {}) => {
+  const { openDialog } = useDialog();
+
+  return useSWRMutation<void, Error, string, SendEmailSchema>(
+    getEmailPathname(),
+    // (key, { arg }) => emailAPI.send(arg),
+    (key, { arg }) => {
+      console.log(arg);
+      return Promise.reject(new Error("test"));
+    },
+    {
+      onSuccess: () => {
+        openDialog({
+          title: "已送出邀請",
+          content: (
+            <>
+              <Image src={contractDoneImg} alt="contract done" />
+              <p>請耐心等候夥伴的回應</p>
+            </>
+          ),
+          cancelBtnProps: { className: "hidden" },
+        });
+        onSuccess?.();
+      },
+      onError: () => {
+        openDialog({
+          title: "哎呀！有不明錯誤",
+          content: <Image src={contractErrorImg} alt="contract error" />,
+          cancelBtnProps: { className: "hidden" },
+          confirmText: "再試一次",
+        });
+      },
+    }
+  );
+};
---
+++ services/emails/api.ts
@@ -0,0 +1,28 @@
+import { ZodError } from 'zod';
+
+import { HttpError, mutations } from '@/utils/http';
+
+import { sendEmailSchema, SendEmailSchema } from './schema';
+
+export const getEmailPathname = () => '/emails';
+
+interface EmailAPIType {
+  send: (source: SendEmailSchema) => Promise<void>;
+}
+
+export const emailAPI: EmailAPIType = {
+  send: (source) => {
+    try {
+      const request = sendEmailSchema.parse(source);
+
+      return mutations.post(getEmailPathname(), request);
+    } catch (error: unknown) {
+      if (error instanceof ZodError) {
+        throw new HttpError(400, { message: error.issues[0].message });
+      }
+      throw new HttpError(400, {
+        message: '信件發送失敗，請稍後再試！',
+      });
+    }
+  },
+};
