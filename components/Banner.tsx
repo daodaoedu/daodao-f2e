516b5953b7de73de9231d5293ed92f75c8795e40
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-08-10 22:44:24 +0800

    refactor(banner): update to learning marathon 2025

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,64 +1,182 @@
-import { BookOpenCheck, Network, Users } from 'lucide-react';
-import Link from 'next/link';
-import { Button } from './ui/button';
-import { Text, Title } from './ui/typography';
+import Image from '@/shared/components/Image';
 
-const Banner = () => (
-  <div className="relative flex flex-col items-center justify-center bg-[#1c3c46] min-h-svh text-center overflow-hidden p-4 sm:p-6 md:p-8">
-    <video
-      autoPlay
-      muted
-      loop
-      playsInline
-      preload="auto"
-      className="absolute top-0 left-0 w-full h-full object-cover"
-      poster="/assets/daodao-banner.webp"
+import LearningMarathonImgDesktopGroup from '@/public/assets/learning-marathon/2025S1-desktop-group.png';
+import LearningMarathonImgDesktopIcon1 from '@/public/assets/learning-marathon/2025S1-desktop-icon-1.png';
+import LearningMarathonImgDesktopIcon2 from '@/public/assets/learning-marathon/2025S1-desktop-icon-2.png';
+import LearningMarathonImgDesktopIcon3 from '@/public/assets/learning-marathon/2025S1-desktop-icon-3.png';
+import LearningMarathonImgDesktopIcon4 from '@/public/assets/learning-marathon/2025S1-desktop-icon-4.png';
+import LearningMarathonImgDesktopIcon5 from '@/public/assets/learning-marathon/2025S1-desktop-icon-5.png';
+import LearningMarathonImgDesktopIcon6 from '@/public/assets/learning-marathon/2025S1-desktop-icon-6.png';
+import LearningMarathonImgDesktopGirl from '@/public/assets/learning-marathon/2025S1-desktop-girl-1.png';
+import LearningMarathonImgDesktopBoy from '@/public/assets/learning-marathon/2025S1-desktop-boy-1.png';
+import LearningMarathonImgDesktopBg from '@/public/assets/learning-marathon/2025S1-desktop-bg.png';
+import LearningMarathonImgMobile from '@/public/assets/learning-marathon/2025S1-mobile@2x.png';
+
+import { usePromotion } from '@/contexts/Promotion';
+import { cn } from '@/utils/cn';
+
+const Banner = ({ children }: React.PropsWithChildren) => {
+  const { isShowPromotionBar } = usePromotion();
+
+  return (
+    <div
+      className={cn(
+        'mt-16 md:h-[calc(100vw/1.6)] h-[calc(100vw/0.6428)] relative overflow-hidden',
+        isShowPromotionBar && 'mt-24'
+      )}
     >
-      <source src="/assets/daodao-banner.webm" type="video/webm" />
-    </video>
-    <div className="absolute inset-0 bg-black/30" />
-
-    <div className="relative z-10 pt-28 flex flex-col items-center justify-center flex-grow w-full">
-      <Title className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-white">
-        找不到學習方向？
-        <span className="block text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-500 py-2">
-          找出屬於你的學習小島！
-        </span>
-      </Title>
-
-      <Text className="body-md mb-8 text-center text-yellow-300 text-balance">
-        完成測驗，立即獲得個人化建議與資源推薦，學習不再迷路！
-      </Text>
-
-      <Button
-        size="lg"
-        className="text-lg font-bold bg-yellow-400 text-black hover:bg-yellow-500"
-        asChild
-      >
-        <Link href="/quiz">立即開始測驗</Link>
-      </Button>
-    </div>
-
-    <div className="hidden sm:block relative z-10 mt-auto w-full max-w-3xl">
-      <div className="flex flex-row items-center justify-around gap-x-8 text-gray-200 text-lg rounded-lg bg-black/30 backdrop-blur-sm p-6">
-        <div className="flex flex-col items-center gap-1">
-          <BookOpenCheck className="w-8 h-8 text-yellow-400" />
-          <span className="font-bold text-3xl">800+</span>
-          <span className="text-base text-gray-300">學習資源</span>
+      <div className="hidden md:block">
+        <div className="absolute inset-0">
+          <Image
+            src={LearningMarathonImgDesktopBg.src}
+            alt="島島盃 - 學習馬拉松 2025 春季賽"
+            height="inherit"
+            background="linear-gradient(#fcfefe 10%, #e0f1f2 40%)"
+            borderRadius="0"
+          />
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <Users className="w-8 h-8 text-yellow-400" />
-          <span className="font-bold text-3xl">200+</span>
-          <span className="text-base text-gray-300">位夥伴</span>
+        <div
+          className={cn(
+            'absolute bottom-0 left-1/2 -translate-x-1/2 w-[30%] aspect-[433/427]',
+            'opacity-0 animate-fade-in animate-duration-700 animate-delay-200'
+          )}
+        >
+          <Image
+            src={LearningMarathonImgDesktopGroup.src}
+            alt="揪團"
+            height="inherit"
+            borderRadius="0"
+          />
         </div>
-        <div className="flex flex-col items-center gap-1">
-          <Network className="w-8 h-8 text-yellow-400" />
-          <span className="font-bold text-3xl">20+</span>
-          <span className="text-base text-gray-300">個揪團</span>
+        <div
+          className={cn(
+            'absolute top-[39%] -left-[2.8%] w-[18%] aspect-[271/404]',
+            '-translate-x-full animate-slide-x-in -animate-distance-full animate-duration-1100 animate-delay-500'
+          )}
+        >
+          <Image
+            src={LearningMarathonImgDesktopBoy.src}
+            alt="男角色"
+            height="inherit"
+            borderRadius="0"
+          />
+        </div>
+        <div
+          className={cn(
+            'absolute top-[33%] -right-[1.6%] w-[18%] aspect-[256/351]',
+            'translate-x-full animate-slide-x-in animate-duration-1100 animate-delay-1100'
+          )}
+        >
+          <Image
+            src={LearningMarathonImgDesktopGirl.src}
+            alt="女角色"
+            height="inherit"
+            borderRadius="0"
+          />
+        </div>
+        <div
+          className={cn(
+            'absolute top-[14%] left-[27.5%] w-[5%] aspect-[75/77]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-200'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-200">
+            <Image
+              src={LearningMarathonImgDesktopIcon5.src}
+              alt="icon5"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'absolute top-[19.5%] left-[21%] w-[6%] aspect-[91/131]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-1700'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-1700">
+            <Image
+              src={LearningMarathonImgDesktopIcon3.src}
+              alt="icon3"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'absolute top-[45.5%] left-[27.5%] w-[4.8%] aspect-[70/39]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-500'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-1100">
+            <Image
+              src={LearningMarathonImgDesktopIcon1.src}
+              alt="icon1"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'absolute top-[16.5%] right-[24.5%] w-[4.7%] aspect-[68/112]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-1300'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-1900">
+            <Image
+              src={LearningMarathonImgDesktopIcon4.src}
+              alt="icon4"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'absolute top-[29%] right-[18.5%] w-[5.9%] aspect-[85/72]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-500'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-300">
+            <Image
+              src={LearningMarathonImgDesktopIcon2.src}
+              alt="icon2"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
+        </div>
+        <div
+          className={cn(
+            'absolute top-[40%] right-[20.5%] w-[11%] aspect-[160/153]',
+            'opacity-0 animate-fade-in animate-duration-500 animate-delay-1900'
+          )}
+        >
+          <div className="animate-oscillate animate-delay-1300">
+            <Image
+              src={LearningMarathonImgDesktopIcon6.src}
+              alt="icon6"
+              height="inherit"
+              borderRadius="0"
+            />
+          </div>
         </div>
       </div>
+      <div className="md:hidden">
+        <Image
+          src={LearningMarathonImgMobile.src}
+          alt="島島盃 - 學習馬拉松 2025 春季賽"
+          height="inherit"
+          background="linear-gradient(#fcfefe 10%, #e0f1f2 40%)"
+          borderRadius="0"
+        />
+      </div>
+      {children}
     </div>
-  </div>
-);
+  );
+};
 
 export default Banner;
