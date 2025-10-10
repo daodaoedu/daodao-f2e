829a367a10ffc36cdcf418dd0d2682d0ddf33ab8
xiaoxu <innocent0705@gmail.com>
2025-10-10 18:02:31 +0800

    feat: implement IdeaDetail page with new components and error handling

---
+++ widgets/idea-detail/index.ts
@@ -0,0 +1,6 @@
+export { IdeaDetailWidget } from './ui/IdeaDetailWidget';
+export { IdeaHeader } from './ui/IdeaHeader';
+export { IdeaContent } from './ui/IdeaContent';
+export { IdeaStats } from './ui/IdeaStats';
+export { LoadingState } from './ui/LoadingState';
+export { ErrorState } from './ui/ErrorState';
