90e17873d45e0602b7692610cf8b796fb90dfeb2
Johnson Mao <86179381+JohnsonMao@users.noreply.github.com>
2025-10-20 23:39:38 +0800

    feat(seo): add metadata about page

--- app/[language]/(guest)/(default-layout)/about/page.tsx
+++ app/[language]/(guest)/(default-layout)/about/page.tsx
@@ -1,5 +1,12 @@
+import { Metadata } from 'next';
 import { Paper } from '@/shared/ui/wrapper';
 import { AboutDaoDao, Vision, Mission, ContactUs } from '@/widgets/about';
+
+export const metadata: Metadata = {
+  title: '關於島島',
+  description:
+    '島島阿學是為「相信學習可以不一樣的人」所打造的學習平台。以科技與社群，匯集學習經驗、資源、人脈，並提供個人化學習管理與技能展現的工具，賦予每個人掌握學習旅程的能力。這裡，是個人成長與集體智慧交會的所在。',
+};
 
 export default async function AboutPage() {
   return (
