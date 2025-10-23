4e4af61bd8a914bbd4873afc6d8ffe650c51b432
xiaoxu <innocent0705@gmail.com>
2025-10-24 04:29:29 +0800

    fix(resources): use user.id instead of user._id in ContributorInfo
        ## Why is this necessary?
        - User schema has been standardized to use id field instead of _id
        - ContributorInfo component still references deprecated _id field
        - This causes incorrect partner detail page navigation
    
        ## How does it address?
        - Update ContributorInfo partner detail link to use user.id
        - Ensure consistent user field reference across the application

--- features/resources/components/ContributorInfo.tsx
+++ features/resources/components/ContributorInfo.tsx
@@ -60,7 +60,7 @@ export default function ContributorInfo({ user }: ContributorInfoProps) {
           <h3 className="body-lg mb-2 font-bold text-basic-500">簡介</h3>
           <p className="mb-2 text-basic-500">{user.selfIntroduction}</p>
           <Button type="button" variant="link" className="-mx-2 px-2" asChild>
-            <CustomLink href={`/partner/detail?id=${user._id}`}>了解更多</CustomLink>
+            <CustomLink href={`/partner/detail?id=${user.id}`}>了解更多</CustomLink>
           </Button>
         </div>
       </div>
